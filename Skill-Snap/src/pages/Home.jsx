9e167640523fcf1a007de962519c2ceddc9f3a2b
riyap03 <riyapanwar0307@gmail.com>
2025-08-12 23:44:45 +0530

    some changes

--- Skill-Snap/src/pages/Home.jsx
+++ Skill-Snap/src/pages/Home.jsx
@@ -6,10 +6,10 @@ function Home(){
 
     return(
         <>
-        
+        <div className="home-container">
         <h1>Hey buddies ! Welcome to Skill Snap</h1>
-        <p>Track your learning journey and build your portfolio .</p>
-        
+        <p> Your journey to showcase skills beautifully starts here.</p>
+        </div>
         </>
     );
 }
